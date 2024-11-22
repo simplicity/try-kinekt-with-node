835cce6980fcedb1604a45cc3fc581a135a8adfa
samu <1936709+samu@users.noreply.github.com>
2024-11-22 09:02:43 +0100

    cleanup

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,6 @@
 import { consoleLogger, createPipeline, logger, notFound, serve } from "kinekt";
 import { getUser } from "./endpoints/getUser";
 
-const notFoundEndpoint = {
-  pipeline: createPipeline(notFound(), logger()),
-};
+const notFoundEndpoint = { pipeline: createPipeline(notFound(), logger()) };
 
 serve([notFoundEndpoint, getUser], consoleLogger);
